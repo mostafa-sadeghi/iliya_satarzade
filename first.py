3a9b930d0ead7347280739699e198915550d2a27
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-11 22:58:00 +0330

    sadeghi

--- first.py
+++ first.py
@@ -1,3 +1,65 @@
-print("hello every body")
-print("welcome to our python class")
-print("we are going to learn every things about python")
+# print("hello every body")
+# print("welcome to our python class")
+# print("we are going to learn every things about python")
+# comment
+
+# primitive data types in python
+# numbers      string         boolean
+# numbers: int      float       complex
+# string str
+# bool    True     False
+
+x = 1
+# how to name a variable in python:
+# any combination of alphebetical chars and numbers and _
+# except variables can not start with numbers
+
+# number1 = 10
+# number_1 = 12
+# number_onr = 15
+# _number = 13
+# 4number = 3  # syntax error
+# number one = 4 # Syntax error
+
+# snake case : number_one = 1
+# camel case : numberOne = 1
+# big camel case : NumberOne = 1
+
+
+# x = 1
+# print("x type is:", type(x))
+
+# x = 1.5
+# print("x type is:", type(x))
+
+
+# + - * / %
+
+# string = "abcdefghi"
+# print("string type is:", type(string))
+# string = 'abcdefghi'
+# print("string type is:", type(string))
+# string = ''
+# print("string type is:", type(string))
+# string = 'a'
+# print("string type is:", type(string))
+# string = "a"
+# print("string type is:", type(string))
+
+# string = "abcdefghi"
+# print("first char is:", string[0])
+# print("first char is:", string[1])
+# print("third char is:", string[2])
+# print("forth char is:", string[3])
+# print("second char is:", string[4])
+# print("second char is:", string[5])
+# print("second char is:", string[6])
+# print("second char is:", string[7])
+# print("second char is:", string[8])
+# print("second char is:", string[9])  # IndexError: string index out of range
+
+# name = input('enter your name: ')
+# print(type(name))
+# print("name is:", name)
+
+# exerice : write a program that gets user's name and prints all chars reversley
